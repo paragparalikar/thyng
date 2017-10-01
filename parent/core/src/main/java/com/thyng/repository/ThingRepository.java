a9bc121f6867b580309a507057803c99e3b1014d
parag_paralikar <parag_paralikar@PUNE-LAP0607.nuance.com>
2017-10-01 16:03:59 +0530

    na

--- parent/core/src/main/java/com/thyng/repository/ThingRepository.java
+++ parent/core/src/main/java/com/thyng/repository/ThingRepository.java
@@ -1,11 +1,22 @@
 package com.thyng.repository;
 
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.querydsl.core.types.dsl.StringPath;
+import com.thyng.model.QThing;
+import com.thyng.model.Template;
 import com.thyng.model.Thing;
 
 @RepositoryRestResource(collectionResourceRel = "things", path = "things")
-public interface ThingRepository extends PagingAndSortingRepository<Thing, String>{
+public interface ThingRepository extends PagingAndSortingRepository<Thing, String>,QueryDslPredicateExecutor<Template>, QuerydslBinderCustomizer<QThing>  {
 
+	@Override
+	default public void customize(QuerydslBindings bindings, QThing root) {
+		bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+	}
+	
 }
