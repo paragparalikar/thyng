d25ecd2348a70ee9727124f2c790f42e7f16cc82
parag_paralikar <parag_paralikar@PUNE-LAP0607.nuance.com>
2017-10-13 17:28:48 +0530

    na

--- parent/discovery-service/src/main/java/com/thyng/ThyngDiscoveryApplication.java
+++ parent/discovery-service/src/main/java/com/thyng/ThyngDiscoveryApplication.java
@@ -3,8 +3,10 @@ package com.thyng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableConfigServer
+@EnableEurekaServer
 @SpringBootApplication
 public class ThyngDiscoveryApplication {
 
