0ba6491724386c06702c604d4d8dfbeb454301a8
parag_paralikar <parag_paralikar@PUNE-LAP0607.nuance.com>
2017-10-13 20:24:44 +0530

    na

--- parent/gateway-service/src/main/java/com/thyng/ThyngGatewayApplication.java
+++ parent/gateway-service/src/main/java/com/thyng/ThyngGatewayApplication.java
@@ -2,12 +2,12 @@ package com.thyng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy	
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ThyngGatewayApplication  {
 	
 	public static void main(String[] args) {
