b2e75caa8385c4cbc3e872bc707d8e2564ddb249
parag_paralikar <parag_paralikar@PUNE-LAP0607.nuance.com>
2017-10-15 20:13:12 +0530

    na

--- parent/authorization-service/src/main/java/com/thyng/security/oauth2/AuthorizationServerConfiguration.java
+++ parent/authorization-service/src/main/java/com/thyng/security/oauth2/AuthorizationServerConfiguration.java
@@ -1,5 +1,7 @@
 package com.thyng.security.oauth2;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +11,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -17,12 +21,15 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
 	@Value("${spring.application.name}")
 	private String resourceId;
-	
+
 	@Autowired
 	private TokenStore tokenStore;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtAccessTokenConverter accessTokenConverter;
 
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
@@ -36,8 +43,12 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 				.redirectUris("https://www.getpostman.com/oauth2/callback");
 	}
 
+	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore).approvalStoreDisabled();
+		TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+		enhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter));
+		endpoints.tokenStore(tokenStore).accessTokenConverter(accessTokenConverter).tokenEnhancer(enhancerChain)
+				.authenticationManager(authenticationManager);
 	}
-	
+
 }
